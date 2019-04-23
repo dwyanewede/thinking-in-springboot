537e81c215d1cc278b08af143ed97d671131e200
dwyanwede <shang_sk@163.com>
2019-04-23 11:25:46 +0800

    first commit

---
+++ src/main/java/think/in/spring/boot/thinkspringboot/ThinkSpringBootApplication.java
@@ -0,0 +1,13 @@
+package think.in.spring.boot.thinkspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThinkSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/think/in/spring/boot/thinkspringboot/ThinkSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package think.in.spring.boot.thinkspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThinkSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
