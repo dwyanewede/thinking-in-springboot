839912c6154b0ef46e7587ad0f173a411f793491
dwyanwede <shang_sk@163.com>
2019-04-26 11:43:18 +0800

    条件装配

---
+++ src/main/java/think/in/spring/boot/autoconfiguration/ConditionalOnSystemProperty.java
@@ -0,0 +1,31 @@
+package think.in.spring.boot.autoconfiguration;
+
+
+import org.springframework.context.annotation.Conditional;
+
+import java.lang.annotation.*;
+
+/**
+ * Java 系统属性 条件判断
+ *
+ * @author sxs
+ * @since 2018/5/15
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE, ElementType.METHOD })
+@Documented
+@Conditional(OnSystemPropertyCondition.class)
+public @interface ConditionalOnSystemProperty {
+
+    /**
+     * Java 系统属性名称
+     * @return
+     */
+    String name();
+
+    /**
+     * Java 系统属性值
+     * @return
+     */
+    String value();
+}
