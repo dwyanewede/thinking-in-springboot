18a98ab8d122bc4c3ee226fd816ba39c7ed9a7e4
dwyanwede <shang_sk@163.com>
2019-04-26 11:43:40 +0800

    条件装配选择器实现

---
+++ src/main/java/think/in/spring/boot/autoconfiguration/HelloWorldImportSelector.java
@@ -0,0 +1,17 @@
+package think.in.spring.boot.autoconfiguration;
+
+import org.springframework.context.annotation.ImportSelector;
+import org.springframework.core.type.AnnotationMetadata;
+
+/**
+ * HelloWorld {@link ImportSelector} 实现
+ *
+ * @author sxs
+ * @since 2018/5/14
+ */
+public class HelloWorldImportSelector implements ImportSelector {
+    @Override
+    public String[] selectImports(AnnotationMetadata importingClassMetadata) {
+        return new String[]{HelloWorldConfiguration.class.getName()};
+    }
+}
