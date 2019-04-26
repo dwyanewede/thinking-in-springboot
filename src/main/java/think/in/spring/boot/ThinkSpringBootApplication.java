28997bd02448637dcda433b08401bea12eb799a5
dwyanwede <shang_sk@163.com>
2019-04-26 11:41:28 +0800

    启动类注释修改

--- src/main/java/think/in/spring/boot/ThinkSpringBootApplication.java
+++ src/main/java/think/in/spring/boot/ThinkSpringBootApplication.java
@@ -2,7 +2,6 @@ package think.in.spring.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import think.in.spring.boot.config.ReactiveWebConfiguration;
 
 @SpringBootApplication(scanBasePackages = "think.in.spring.boot.config")
 public class ThinkSpringBootApplication {
