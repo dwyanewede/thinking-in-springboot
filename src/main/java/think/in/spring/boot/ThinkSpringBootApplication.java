63a299f433676bb6a2607ab904ab7d9d4a81a306
dwyanwede <shang_sk@163.com>
2019-05-16 08:33:55 +0800

    模拟启动环境变量设置

--- src/main/java/think/in/spring/boot/ThinkSpringBootApplication.java
+++ src/main/java/think/in/spring/boot/ThinkSpringBootApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ThinkSpringBootApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("spring-think-spring-boot-author-name","shangxiansheng");
 		SpringApplication.run(ThinkSpringBootApplication.class, args);
 	}
 
