f95bf2442a2e83b8d2ee63e699bd62d4a24c14f5
dwyanwede <shang_sk@163.com>
2019-05-23 11:59:21 +0800

    aoontation方式配置durid相关filter和servlet

--- src/main/java/think/in/spring/boot/config/DataSourceConfiguration.java
+++ src/main/java/think/in/spring/boot/config/DataSourceConfiguration.java
@@ -54,4 +54,32 @@ public class DataSourceConfiguration {
 //        return new SqlSessionTemplate(sqlSessionFactory);
 //    }
 
+
+
+
+
+    // druid 相关 filter 和 servlet 配置
+//    @Bean
+//    public ServletRegistrationBean statViewServlet(){
+//        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(),"/druid/*");
+//        servletRegistrationBean.addInitParameter("allow","127.0.0.1");  //设置ip白名单
+//        servletRegistrationBean.addInitParameter("deny","192.168.0.19");//设置ip黑名单，优先级高于白名单
+//        //设置控制台管理用户
+//        servletRegistrationBean.addInitParameter("loginUsername","root");
+//        servletRegistrationBean.addInitParameter("loginPassword","root");
+//        //是否可以重置数据
+//        servletRegistrationBean.addInitParameter("resetEnable","false");
+//        return servletRegistrationBean;
+//    }
+//
+//    @Bean
+//    public FilterRegistrationBean statFilter(){
+//        //创建过滤器
+//        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
+//        //设置过滤器过滤路径
+//        filterRegistrationBean.addUrlPatterns("/*");
+//        //忽略过滤的形式
+//        filterRegistrationBean.addInitParameter("exclusions","*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+//        return filterRegistrationBean;
+//    }
 }
