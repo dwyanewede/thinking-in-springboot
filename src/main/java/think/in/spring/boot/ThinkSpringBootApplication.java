0fa25f671b799ab8ed3c92b5612adda95d83ca99
dwyanwede <shang_sk@163.com>
2019-05-23 11:29:44 +0800

    扫描项目下面的 @WebFilter 和 @WebServlet

--- src/main/java/think/in/spring/boot/ThinkSpringBootApplication.java
+++ src/main/java/think/in/spring/boot/ThinkSpringBootApplication.java
@@ -2,9 +2,12 @@ package think.in.spring.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 //@SpringBootApplication(scanBasePackages = "think.in.spring.boot.config")
 @SpringBootApplication
+// 扫描项目下面的 @WebFilter 和 @WebServlet
+@ServletComponentScan
 public class ThinkSpringBootApplication {
 
 	public static void main(String[] args) {
